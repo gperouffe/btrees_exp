4c5d0e8f5c89276cea699d3a1b90b3a0e0aad580
Guillaume Perouffe <g.perouffe@gmail.com>
2016-06-16 15:00:17 +0200

    Version with repetitions

--- bt_test.cc
+++ bt_test.cc
@@ -8,6 +8,7 @@ int main(int argc, char* argv[])
 {
     int Mmax = atoi(argv[1]);
     int nOps = atoi(argv[2]);
+    int reps = atoi(argv[3]);
     
     ofstream out("manyBT.cc");
     out << "#include \"btree_set.h\"" << endl;
@@ -16,33 +17,36 @@ int main(int argc, char* argv[])
     out << "#include <time.h>" << endl;
     out << "using namespace std;" << endl;
     
-    for(int i = 16; i <= Mmax; i*=2)
+    for(int i = 16; i <= Mmax; ++i)
         out << "typedef btree::btree_set<long,less<long>,allocator<long>," << i << "> BT" << i << ";" << endl;
         
     out << "int main(int argc, char* argv[]){" << endl;
     out << "    clock_t t;" << endl;
     out << "    srand(time(NULL)*4);" << endl;
     out << "    cout << \"Number of operations: \" << " << nOps << " << endl;" << endl;
-    out << "    cout << setw(8) << \"M,\" << setw(11) << \"Insert,\" << setw(11) << \"Find,\" << setw(11) << \"Delete,\" << endl;" << endl;
+    out << "    cout << setw(8) << \"M,\" << setw(11) << \"Insert,\" << setw(11) << \"Find,\" << setw(11) << \"Delete,\" << setw(11) << \"Node size\" << endl;" << endl;
     
-    for(int i = 16; i <= Mmax; i*=2)
+    for(int i = 16; i <= Mmax; ++i)
     {
-        out << "    cout << setw(8) << \"" << i << ",\";" << endl;
-        out << "    BT" << i << " bt" << i << ";" << endl;
-        for(int k = 0; k < 3; ++k)
-        {
-            
-            out << "    t = clock();" << endl;
-            out << "    for(long i = 0; i < " << nOps << "; ++i)" << endl;
-            if(k == 0)
-                out << "        bt" << i << ".insert(rand());" << endl;
-            if(k == 1)
-                out << "        bt" << i << ".find(rand());" << endl;
-            if(k == 2)
-                out << "        bt" << i << ".erase(rand());" << endl;
-            out << "    cout << setw(11) << (float)(clock() - t)/CLOCKS_PER_SEC << \", \";" << endl;
-        }
-        out << "cout << endl;" << endl;
+            out << "    BT" << i << " bt" << i << ";" << endl;
+            out << "    for(int j = 0; j < " << reps <<"; ++j){" << endl;
+            out << "        bt" << i << ".clear();" << endl;
+            out << "        cout << setw(8) << \"" << i << ",\";" << endl;
+            for(int k = 0; k < 3; ++k)
+            {
+                
+                out << "        t = clock();" << endl;
+                out << "        for(long i = 0; i < " << nOps << "; ++i)" << endl;
+                if(k == 0)
+                    out << "            bt" << i << ".insert(rand());" << endl;
+                if(k == 1)
+                    out << "            bt" << i << ".find(rand());" << endl;
+                if(k == 2)
+                    out << "            bt" << i << ".erase(rand());" << endl;
+                out << "    cout << setw(11) << (float)(clock() - t)/CLOCKS_PER_SEC << \", \";" << endl;
+            }
+            out << "cout << setw(11) << " << sizeof(long) * i << " << endl;" << endl;
+            out << "    }" << endl;
     }
     
     out << "    cout << endl;" << endl;
